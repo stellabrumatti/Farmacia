cafd94e2b97a2d490ef89a3862b601a29c262be6
stella-brumatti <stella.wma@gmail.com>
2025-10-27 13:40:16 -0300

    Implementa CRUD de Produto e relacionamento com Categoria

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,50 +1,72 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-
-
-
 @Entity
 @Table(name = "tb_categorias")
 public class Categoria {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 
-	@NotBlank(message = "O Atributo Descrição é obrigatório")
-	@Size(min = 3, max = 100, message = "O Atributo Descrição deve conter no mínimo 3 e no máximo 100 caracteres")
-	private String nome;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
+    @NotBlank(message = "O Atributo Nome é obrigatório")
+    @Size(min = 3, max = 100, message = "O Atributo Nome deve conter no mínimo 3 e no máximo 100 caracteres")
+    private String nome;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @NotBlank(message = "O Atributo Descrição é obrigatório")
+    @Size(min = 3, max = 255, message = "O Atributo Descrição deve conter no mínimo 3 caracteres")
+    private String descricao;
 
-	public String getTipo() {
-		return nome;
-	}
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties(value = "categoria", allowSetters = true)
+    private List<Produto> produtos;
 
-	public void setTipo(String nome) {
-		this.nome = nome;
-	}
-	
-	
-	
-	
-	}
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+}
+
